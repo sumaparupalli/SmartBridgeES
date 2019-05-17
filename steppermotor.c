24a1c6ae26c03e4f719c6b2ee423e83be90f7b7e
sumaparupalli <50615476+sumaparupalli@users.noreply.github.com>
2019-05-17 00:26:48 -0400

    Add files via upload

---
+++ steppermotor.c
@@ -0,0 +1,66 @@
+/*
+ * stepper motor.c
+ *
+ * Created: 16-May-19 3:24:38 PM
+ * Author : admini
+ */ 
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRA = 0X0F; //MAKE PORTA LOWER PINS AS O/P
+	int period = 2;
+    while (1) 
+    {
+		/*rotate stepper motor clockwise with half step  sequence */
+		for(int i = 0;i < 50;i++)
+		{
+			PORTA = 0X09;
+			_delay_ms(period);
+			PORTA = 0X08;
+			_delay_ms(period);
+			PORTA = 0X0C;
+			_delay_ms(period);
+			PORTA = 0X06;
+			_delay_ms(period);
+			PORTA = 0X04;
+			_delay_ms(period);
+			PORTA = 0X02;
+			_delay_ms(period);
+			PORTA = 0X03;
+			_delay_ms(period);
+			PORTA = 0X01;
+			_delay_ms(period);
+		}
+		PORTA = 0X09;  //LAST STEP TO INITIAL POSITION
+			_delay_ms(period);
+			_delay_ms(1000);
+	/*rotate stepper motor anticlockwise with half step  sequence */
+			for(int i = 50;i > 0;i--)
+			{
+				PORTA = 0X01;
+				_delay_ms(period);
+				PORTA = 0X03;
+				_delay_ms(period);
+				PORTA = 0X02;
+				_delay_ms(period);
+				PORTA = 0X04;
+				_delay_ms(period);
+				PORTA = 0X06;
+				_delay_ms(period);
+				PORTA = 0X0C;
+				_delay_ms(period);
+				PORTA = 0X08;
+				_delay_ms(period);
+				PORTA = 0X09;				
+				_delay_ms(period);
+			}
+			PORTA = 0X09;  //LAST STEP TO INITIAL POSITION
+			_delay_ms(period);
+			_delay_ms(1000);
+}
+}
+
